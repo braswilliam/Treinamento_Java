547d3a6a76aa22879564405e1bc17fb1db25d5f4
William B. Pereira <braswilliam@gmail.com>
2023-03-07 02:09:26 -0300

    Tratamento de exceções

--- src/edu/william/java/execoes/aula2/Program.java
+++ src/edu/william/java/execoes/aula2/Program.java
@@ -7,11 +7,22 @@ public class Program {
 
     public static void main(String[] args) {
 
-        methond2();
+        methond1();
         System.out.println("Fim do programa.");
 
 
     }
+
+
+    public static void methond1() {
+
+        System.out.println("***METHOD1 START***");
+
+        methond2();
+
+        System.out.println("***METHOD1 END***");
+    }
+
 
     public static void methond2() {
         Scanner sc = new Scanner(System.in);
@@ -24,10 +35,14 @@ public class Program {
         }
         catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Invalid position.");
+            e.printStackTrace();
+            sc.next();
         }
         catch (InputMismatchException e) {
             System.out.println("Input Error!");
         }
+
+        System.out.println("***METHOD2 END***");
 
         sc.close();
 
