98f9f9038d4a30875b84bfc9cbba8dd0a8b56cad
William B. Pereira <braswilliam@gmail.com>
2023-04-08 20:24:04 -0300

    Att repositórios

---
+++ src/edu/william/java/desafios/desafio1/calculadora.java
@@ -0,0 +1,14 @@
+package edu.william.java.desafios.desafio1;
+
+public class calculadora {
+    
+    
+    
+    public static int fibonacci(int num) {
+        if(num < 2){
+            return 1;
+        }
+
+        return fibonacci(num -1) + fibonacci(num - 2);
+    }
+}
